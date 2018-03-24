069e4c73351ea7209cecb3f0f4d8805475809356
juan <juanmarceloarpi@hotmail.com>
2018-03-24 16:19:06 -0500

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,12 @@
+print('hola')
+if 3>2:
+    print("3 es mayor a dos")
+def hola():
+    print("es es una hola desde funcion")
+hola()
+
+def presentar(lista):
+    for l in lista:
+        print("hola" + l)
+lista=['juan', 'kevin', 'andres']
+presentar(lista)
